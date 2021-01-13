e13b1211b7d3f33bfceea1370c7f73f3f56d3bc7
Lee44 <jlee7772@yahoo.com>
2021-01-13 00:22:48 -0800

    Working On Register.html

---
+++ ecommerce/store/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	#Leave as empty string for base url
+	path('', views.store, name="store"),
+	path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+	path('update_item/', views.updateItem, name="update_item"),
+	path('process_order/', views.processOrder, name="process_order"),
+]
